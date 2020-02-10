d4c888bff1d48823e981361147bd355628fadd3b
mikasaukyou <43193913+mikasaukyou@users.noreply.github.com>
2020-02-10 13:47:43 +0900

    fix c0ban

--- Lyra2RE.h
+++ Lyra2RE.h
@@ -8,6 +8,8 @@ extern "C" {
 void lyra2re_hash(const char* input, char* output);
 void lyra2re2_hash(const char* input, char* output);
 void lyra2rec0ban_hash(const char* input, char* output);
+void lyra2rev2_hash(const char* input, char* output);
+void lyra2revc0ban_hash(const char* input, char* output);
 
 #ifdef __cplusplus
 }
